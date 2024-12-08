270b2ea10b6b6470015b43d1dd36a9a67eb7faba
ca-macbook <achinga.chris@gmail.com>
2024-12-08 16:18:43 +0300

    add layout components and update styles for improved structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
+    './node_modules/preline/preline.js',
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {
