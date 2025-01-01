125aa989fb0f51fd3d2f3ab62908d641fd01bcf0
ca <achinga.chris@gmail.com>
2025-01-02 01:52:55 +0300

    refactor: standardize formatting and improve Google Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,43 +3,43 @@ import Script from "next/script";
 import "./globals.css";
 import PrelineScript from "./components/PrelineScript";
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
 
-const baseURL = 'https://chrisdevcode.co.ke';
-const defaultTitle = 'ChrisDevCode';
-const defaultDescription = 'ChrisDevCode | technologies that may make sense';
-const imagesBaseURL = 'https://chrisdevcode.co.ke';
-const creatorName = 'Chris Achinga';
+const baseURL = "https://chrisdevcode.co.ke";
+const defaultTitle = "ChrisDevCode";
+const defaultDescription = "ChrisDevCode | technologies that may make sense";
+const imagesBaseURL = "https://chrisdevcode.co.ke";
+const creatorName = "Chris Achinga";
 
 export const metadata: Metadata = {
   title: defaultTitle,
   description: defaultDescription,
   keywords: [
-    'Software Developers',
-    'Developers in Mombasa',
-    'Developers in Kenya',
-    'Python Developers',
-    'Django Developers in Kenya',
-    'Django Developers in Mombasa',
-    'Software Engineers',
+    "Software Developers",
+    "Developers in Mombasa",
+    "Developers in Kenya",
+    "Python Developers",
+    "Django Developers in Kenya",
+    "Django Developers in Mombasa",
+    "Software Engineers",
   ],
-  referrer: 'origin-when-cross-origin',
+  referrer: "origin-when-cross-origin",
   creator: creatorName,
   publisher: `${defaultTitle} Technologies`,
   applicationName: defaultTitle,
   alternates: {
-    canonical: '/',
+    canonical: "/",
     languages: {
-      'en-US': '/en-US',
+      "en-US": "/en-US",
     },
   },
   openGraph: {
     title: `${defaultTitle} Technologies`,
-    description: 'Building technologies that may make sense',
+    description: "Building technologies that may make sense",
     url: baseURL,
     siteName: defaultTitle,
     images: [
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
         url: `${imagesBaseURL}/og-alt.png`,
         width: 1800,
         height: 1600,
-        alt: 'My custom alt',
+        alt: "My custom alt",
       },
     ],
     videos: [
@@ -67,8 +67,8 @@ export const metadata: Metadata = {
         url: `${imagesBaseURL}/audio.mp3`,
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: false,
@@ -78,47 +78,47 @@ export const metadata: Metadata = {
       index: true,
       follow: false,
       noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   icons: {
     icon: [
-      { url: '/icon.png' },
-      new URL('/icon.png', baseURL),
-      { url: '/icon-dark.png', media: '(prefers-color-scheme: dark)' },
+      { url: "/icon.png" },
+      new URL("/icon.png", baseURL),
+      { url: "/icon-dark.png", media: "(prefers-color-scheme: dark)" },
     ],
-    shortcut: ['/shortcut-icon.png'],
+    shortcut: ["/shortcut-icon.png"],
     apple: [
-      { url: '/apple-icon.png' },
-      { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
+      { url: "/apple-icon.png" },
+      { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
       {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/apple-touch-icon-precomposed.png',
+        rel: "apple-touch-icon-precomposed",
+        url: "/apple-touch-icon-precomposed.png",
       },
     ],
   },
   manifest: `${imagesBaseURL}/manifest.json`,
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: defaultTitle,
-    description: 'The React Framework for the Web',
-    siteId: '1467726470533754880',
-    creator: '@nextjs',
-    creatorId: '1467726470533754880',
+    description: "The React Framework for the Web",
+    siteId: "1467726470533754880",
+    creator: "@nextjs",
+    creatorId: "1467726470533754880",
     images: [`${imagesBaseURL}/og.png`],
   },
   appleWebApp: {
-    title: 'Apple Web App',
-    statusBarStyle: 'black-translucent',
+    title: "Apple Web App",
+    statusBarStyle: "black-translucent",
     startupImage: [
-      '/assets/startup/apple-touch-startup-image-768x1004.png',
+      "/assets/startup/apple-touch-startup-image-768x1004.png",
       {
-        url: '/assets/startup/apple-touch-startup-image-1536x2008.png',
-        media: '(device-width: 768px) and (device-height: 1024px)',
+        url: "/assets/startup/apple-touch-startup-image-1536x2008.png",
+        media: "(device-width: 768px) and (device-height: 1024px)",
       },
     ],
   },
@@ -129,30 +129,37 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GA_ID = process.env.GA_ID;
+
+  // Log the GA ID to the console
+  // console.log("Google Analytics ID:", GA_ID);
+
   return (
     <html lang="en">
       <body>
         <Header />
-        <main>
-        {children}
-        </main>
+        <main>{children}</main>
         <Footer />
+        <PrelineScript />
+        <SpeedInsights />
+        <Analytics />
+        {GA_ID && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
       </body>
-      <PrelineScript />
-      <SpeedInsights />
-      <Analytics />
-      <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-ZKM8LR9G22"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-ZKM8LR9G22');
-          `}
-        </Script>
     </html>
   );
 }
