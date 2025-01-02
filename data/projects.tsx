e409d3a3b53bfb9ebbf14445a90b0b38b82239a9
ca <achinga.chris@gmail.com>
2025-01-02 20:35:17 +0300

    add: create projects data structure with sample project and form for contacts

---
+++ data/projects.tsx
@@ -0,0 +1,16 @@
+export interface Projects {
+    name: string
+    shortDescription: string
+    description: string
+}
+
+
+const projects: Projects[] = [
+    {
+        name: 'Project 1',
+        shortDescription: 'A project that does something',
+        description: 'This project'
+    }
+]
+
+export default projects
